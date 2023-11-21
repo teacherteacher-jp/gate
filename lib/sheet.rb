220d8410e52ce3c967805bd918f29e1ca35d13e2
june29 <june29.jp@gmail.com>
2023-11-21 23:08:44 +0900

    シートからemailを探し、見つかればnameを記録する

---
+++ lib/sheet.rb
@@ -0,0 +1,34 @@
+require "google/apis/sheets_v4"
+
+module Sheet
+  SHEET_ID = ENV["GOOGLE_SHEETS_SHEET_ID"]
+
+  class << self
+    def find_email_and_write_name(email, name)
+      api = Google::Apis::SheetsV4::SheetsService.new
+      authorizer = Google::Auth::ServiceAccountCredentials.make_creds(
+        scope: %w[https://www.googleapis.com/auth/spreadsheets]
+      )
+      authorizer.fetch_access_token!
+      api.authorization = authorizer
+
+      values = api.get_spreadsheet_values(SHEET_ID, "A:C", value_render_option: "FORMULA").values
+      values.each.with_index(1) do |(e, u, t), i|
+        p [e, u, t, i]
+        if e == email
+          api.update_spreadsheet_value(
+            SHEET_ID,
+            "B#{i}:C#{i}",
+            Google::Apis::SheetsV4::ValueRange.new(
+              values: [[name, Time.current.strftime("%Y-%m-%d %H:%M:%S")]]
+            ),
+            value_input_option: "RAW"
+          )
+          return true
+        end
+      end
+
+      false
+    end
+  end
+end
