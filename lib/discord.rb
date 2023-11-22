7aeef54b3c6572764808d0c4c5415640664b4799
june29 <june29.jp@gmail.com>
2023-11-22 22:13:48 +0900

    Discordサーバーに招待してRoleを付与する処理を実装する

--- lib/discord.rb
+++ lib/discord.rb
@@ -5,12 +5,29 @@ class Discord
   def initialize(bot_token)
     @connection = Faraday.new(
       url: BASE_URL,
-      headers: { "Authorization" => "Bot #{bot_token}" }
+      headers: {
+        "Authorization" => "Bot #{bot_token}",
+        "Content-Type" => "application/json"
+      }
     )
   end
 
   def servers
     JSON.parse(get("/users/@me/guilds").body)
+  end
+
+  def roles(server_id)
+    JSON.parse(get("/guilds/#{server_id}/roles").body)
+  end
+
+  def invite(server_id:, user_id:, user_token:)
+    @connection.put(BASE_PATH + "/guilds/#{server_id}/members/#{user_id}") do |request|
+      request.body = { access_token: user_token }.to_json
+    end
+  end
+
+  def add_role(server_id:, user_id:, role_id:)
+    @connection.put(BASE_PATH + "/guilds/#{server_id}/members/#{user_id}/roles/#{role_id}")
   end
 
   def get(path)
